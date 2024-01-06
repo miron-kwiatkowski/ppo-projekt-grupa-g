6f6a950c23f681280e3fc61b15b3446be971cc10
miron-kwiatkowski <miron.kwiatkowski@studenci.collegiumwitelona.pl>
2024-01-06 19:01:35 +0100

    first commit/generator test

---
+++ projekt/Generator.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Generator{
+    private static final List<String> maleFirstNames = Arrays.asList("Mietek");
+    private static final List<String> femaleFirstNames = Arrays.asList("Karyna");
+    private static final List<String> origin = Arrays.asList("spod Lidla");
+    
+}
