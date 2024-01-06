20519f8570b3c4e87dea60dc5ced098732cbb5c3
Ewakubera <146948857+Ewakubera@users.noreply.github.com>
2024-01-06 19:11:06 +0100

    Update Hobo.java

--- projekt/Hobo.java
+++ projekt/Hobo.java
@@ -1,3 +1,30 @@
-public class Hobo {
-    
-}
+class Hobo {
+    private String name;
+    private String gender;
+    private int healthPoints;
+    private int manaPoints;
+
+    public Hobo(String name) {
+        this.gender = gender;
+        this.name = name;
+        this.healthPoints = 100;
+        this.manaPoints = 100;
+        }
+        
+
+public String getName() {
+        return name;
+        }
+
+public int getHealthPoints() {
+        return healthPoints;
+        }
+    public int getManaPoints() {
+        return manaPoints;
+    }
+public void takeHit(Integer hitPoints) {
+        this.healthPoints = this.healthPoints - hitPoints;
+        }
+public boolean isDead() {
+        return healthPoints <= 0;
+        }}
