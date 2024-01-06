ee8ddd867bdf3f09944139e432671e8ed72631f2
Amelia Wałek (HeroHorrendous) <147624792+AmeliaWalek@users.noreply.github.com>
2024-01-06 19:21:20 +0100

    Update Generator.java

--- projekt/Generator.java
+++ projekt/Generator.java
@@ -4,8 +4,8 @@ import java.util.Random;
 import java.util.ArrayList;
 
 public class Generator{
-    private static final List<String> maleFirstNames = Arrays.asList("Mietek");
-    private static final List<String> femaleFirstNames = Arrays.asList("Karyna");
+    private static final List<String> maleFirstNames = Arrays.asList("Mietek", "Ryszard", "Zdzichu", "Mścichuj", "Moczywór", "Andrzej", "Józek", "Stachu", "Bolek", "Marian", "Brajan", "Tadek", "Ferdek", "Bożydar");
+    private static final List<String> femaleFirstNames = Arrays.asList("Karyna", "Nikola", "Andżelika", "Dżeskia", "Elka", "Helena", "Halina", "Bożenka", "Jebodyda", "Mścisława", "Kaśka", "Klaudia", "Żanetka", "Renata", "Mariola");
     private static final List<String> origin = Arrays.asList("spod Lidla");
     
 }
