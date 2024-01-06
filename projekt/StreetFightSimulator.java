d66186f7f7ad7645aa53a31807e974f4276bb89f
Ewakubera <146948857+Ewakubera@users.noreply.github.com>
2024-01-06 22:54:46 +0100

    Update StreetFightSimulator.java

--- projekt/StreetFightSimulator.java
+++ projekt/StreetFightSimulator.java
@@ -31,38 +31,3 @@ public class StreetFightSimulator {
                 Fight.fight(player, enemy1);
             }
         }
-
-
-
-        //wybieranie opcji
-            //wyświetl opocje
-    /*private static void executeCommand(Collection collection, String command, String parameter) throws InvalidCommandException {
-        try {
-            switch (command) {
-                case "a": //attack
-                        Activietes.attack();
-                    break;
-                case "spa": //specjal attack
-                    Activietes.specialAttack();
-                    break;
-                case "i": //items
-                    activietes = activietes.items(parameter);
-                    break;
-                case "ia": //ataki z itemkow
-                    collection.clear();
-                    break;
-                case "help": //info co można zrobić
-                    collection.help(parameter);
-                    break;
-                default:
-                    throw new InvalidCommandException();
-            }
-         } catch (InputException e) {
-        new Handler().handle(e);}
-        }*/
-            //sprawdzaj żywotność gracza i przeciwnika
-            //jeśli martwy gracz koniec gry.exe
-            //jeśli przeciwnik martwy dodaj itemki i pogratuluj.exe
-            //generuj następnego przeciwnika i powtórz akcje
-            //walka z bossem po np. 3 przeciwnikach
-            //koniec gry
