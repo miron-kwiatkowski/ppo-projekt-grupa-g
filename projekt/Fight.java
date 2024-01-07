c763e9c5e44873da9f3e215c8efb1d150e653530
Ewakubera <146948857+Ewakubera@users.noreply.github.com>
2024-01-07 16:52:25 +0100

    Update Fight.java

--- projekt/Fight.java
+++ projekt/Fight.java
@@ -3,67 +3,86 @@ import java.util.Scanner;
 public class Fight {
     public static void fight(Hobo player, Hobo enemy) {
         Scanner input = new Scanner(System.in);
+        Items items = new Items();
         System.out.println("Rozpoczynamy walkę!");
 
         while (!player.isDead() && !enemy.isDead()) {
+            System.out.printf("Ty: %d HP %d MP\nPrzeciwnik: %d HP %d MP\n",
+                    player.getHealthPoints(), player.getManaPoints(),
+                    enemy.getHealthPoints(), enemy.getManaPoints());
 
-            // Gracz ma możliwość wyboru
             System.out.println("Co chcesz zrobić? (Wprowadź odpowiednią komendę):");
             System.out.println("a - Atak");
             System.out.println("b - Użyj przedmiotu");
+            System.out.println("c - Specjalny Atak");
+            System.out.println("d - Użyj buffa");
             System.out.println("help - Informacje o dostępnych akcjach");
 
             String command = input.next();
             String parameter = "";
 
-            if (command.equals("b")) {
-                System.out.println("Podaj nazwę przedmiotu:");
+            if (command.equals("b") || command.equals("d")) {
+                System.out.println("Podaj nazwę przedmiotu/buffa:");
                 parameter = input.next();
             }
 
-            playerTurn(player, enemy, command, parameter);
-            if (!enemy.isDead()) {
-                enemyTurn(player, enemy);
-            }
-                        // Wyświetl informacje o graczu i przeciwniku
+            playerTurn(player, enemy, items, command, parameter);
+
+            // Wyświetlanie statystyk po wykonaniu ruchu przez gracza
             System.out.printf("Ty: %d HP %d MP\nPrzeciwnik: %d HP %d MP\n",
                     player.getHealthPoints(), player.getManaPoints(),
                     enemy.getHealthPoints(), enemy.getManaPoints());
+
+            if (!enemy.isDead()) {
+                enemyTurn(player, enemy);
+            }
         }
 
-        // Wyświetl wynik walki
+        // Wyświetlanie statystyk po wykonaniu ruchu przez gracza
+        System.out.printf("Ty: %d HP %d MP\nPrzeciwnik: %d HP %d MP\n",
+                player.getHealthPoints(), player.getManaPoints(),
+                enemy.getHealthPoints(), enemy.getManaPoints());
+
         if (player.isDead()) {
             System.out.println("Przegrałeś walkę! Koniec symulacji.");
         } else {
             System.out.println("Gratulacje! Wygrałeś walkę! Przeciwnik pokonany.");
-            // Dodatkowe działania po pokonaniu przeciwnika, np. zdobycie przedmiotu
         }
     }
 
-    private static void playerTurn(Hobo player, Hobo enemy, String command, String parameter) {
-        Activities activities = new Activities();
+    private static void playerTurn(Hobo player, Hobo enemy, Items items, String command, String parameter) {
+        Help help = new Help();
         try {
             switch (command) {
-                case "a": // attack
-                    activities.attack();
+                case "a": // atak
                     enemy.takeHit(player.getAttackPoints());
                     break;
-                case "b": // item use
-                    activities.itemsUse(parameter);
+                case "b": // użycie przedmiotu na wrogu
+                    items.itemsUse(parameter, enemy);
+                    break;
+                case "c": // specjalny atak
+                    if (player.getManaPoints() >= 10) {
+                        player.specialAttack(enemy);
+                    } else {
+                        System.out.println("Nie masz wystarczająco many do użycia specjalnego ataku!");
+                    }
+                    break;
+                case "d": // użycie buffa
+                    items.itemsBuffs(parameter, player);
                     break;
                 case "help": // info
-                    activities.help("");
+                    help.help("");
                     break;
                 default:
                     System.out.println("Nieprawidłowa komenda!");
             }
-        } finally {
-
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     private static void enemyTurn(Hobo player, Hobo enemy) {
-        // Przeciwnik używa swojego ataku
+        // Enemy uses its attack
         System.out.println(enemy.getName() + " atakuje ciebie!");
         player.takeHit(enemy.getAttackPoints());
     }
