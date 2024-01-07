8d2dd6a70f131b6ec46246c03d172ff9795f004f
Ewakubera <146948857+Ewakubera@users.noreply.github.com>
2024-01-07 15:59:07 +0100

    Add files via upload

---
+++ projekt/Patus.java
@@ -0,0 +1,23 @@
+public class Patus extends Hobo {
+    private int attackPoints;
+
+    public Patus(String name, String gender) {
+        super(name, gender);
+        this.className = "Patus";
+        this.attackPoints = 10;
+    }
+
+    public int getAttackPoints() {
+        return attackPoints;
+    }
+
+    public void attack(Hobo target) {
+        System.out.println(getName() + " dał ci z łokcia\n-" + getAttackPoints() + " HP.");
+        target.takeHit(getAttackPoints());
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nAtakuje z siłą: " + getAttackPoints();
+    }
+}
