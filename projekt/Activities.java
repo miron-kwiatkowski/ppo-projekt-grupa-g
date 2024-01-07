1ef890eefc0146cfb6f7a2dd9ae89699109861e9
Ewakubera <146948857+Ewakubera@users.noreply.github.com>
2024-01-07 15:58:38 +0100

    Update Activities.java

--- projekt/Activities.java
+++ projekt/Activities.java
@@ -1,106 +1,80 @@
-import java.util.function.Predicate;
-
 public class Activities {
 
     public void attack() {
         System.out.println("Atak");
-        // Atak zwykły postaci;
+        //atak zwykły postaci;
     }
 
     public void specialAttack() {
         System.out.println("Specjalny Atak");
-        // Sprawdź klasę, użyj ataku specjalnego (np attack + dodatkowe staty);
+        //sprawdż klase, użyj atak specjalny (np attack + dodatkowe staty);
+
     }
 
-    public Activities itemsUse(String x) {
-        // Atakujące przedmioty
-        Predicate<String> useItemCondition = word -> {
-            switch (word) {
-                case "klejpała":
-                    // Sprawdź czy posiada, jeśli tak użyj;
-                    System.out.println("Gracz używa klejpały!");
-                    // Info o obrażeniach
-                    return true;
-                case "tulipan":
-                    // Sprawdź czy posiada, jeśli tak użyj;
-                    System.out.println("Gracz używa tulipana z rozbitej butelki po winie!");
-                    // Info o obrażeniach
-                    return true;
-                case "bombelek":
-                    // Sprawdź czy posiada, jeśli tak użyj;
-                    System.out.println("Gracz używa bombelka");
-                    // Info o obrażeniach
-                    return true;
-                default:
-                    System.out.println("Nie dla psa!");
-                    // Nie ma przedmiotu - błąd;
-                    return false;
-            }
-        };
-
-        if (useItemCondition.test(x)) {
-            // Execute additional actions if the condition is met
+    public Activities itemsUse(String x) { //atakujące
+        switch (x) {
+            case "klejpała":
+                //sprawdż czy posiada, jeśli tak użyj;
+                System.out.println("Gracz używa klejpały!");
+                //info o obrażeniach
+                break;
+            case "tulipan":
+                //sprawdż czy posiada, jeśli tak użyj;
+                System.out.println("Gracz używa tulipana");
+                //info o obrażeniach
+                break;
+            case "bombelek":
+                //sprawdż czy posiada, jeśli tak użyj;
+                System.out.println("Gracz używa bombelka");
+                //info o obrażeniach
+                break;
+            default:
+                System.out.println("Nie dla psa!");
+                //nie ma przedmiotu błąd;
         }
-
         return this;
     }
 
-    public Activities itemsBusts(String x) {
-        // Dodające HP i manę
-        Predicate<String> bustsCondition = word -> {
-            switch (word) {
-                case "Batonik":
-                    // Sprawdź czy posiada, jeśli tak użyj;
-                    System.out.println("Dobry mars byku");
-                    // Info o dodanych statach
-                    return true;
-                case "Amarenka":
-                    // Sprawdź czy posiada, jeśli tak użyj;
-                    System.out.println("Mmm dobra mirabelkowa");
-                    // Info o dodanych statach
-                    return true;
-                default:
-                    System.out.println("Nie dla psa!");
-                    // Nie ma przedmiotu - błąd;
-                    return false;
-            }
-        };
+    public Activities itemsBusts(String x) { // dodające hp i mane
 
-        if (bustsCondition.test(x)) {
-            // Execute additional actions if the condition is met
+        switch (x) {
+            case "Batonik":
+                //sprawdż czy posiada, jeśli tak użyj;
+                System.out.println("Dobry mars byku");
+                //info o dodanych statach
+                break;
+            case "Amarenka":
+                //sprawdż czy posiada, jeśli tak użyj;
+                System.out.println("MMM dobry rocznik");
+                //info o dodanych statach
+                break;
+            default:
+                System.out.println("Nie dla psa!");
+                //nie ma przedmiotu błąd;
         }
+        return this;}
 
-        return this;
-    }
-
-    public Activities help(String x) {
-        // Informacje o dostępnych akcjach
-        Predicate<String> helpCondition = word -> {
-            switch (word) {
+        public Activities help(String x) {
+            switch (x) {
                 case "":
                     System.out.println("Oto co możesz począć:" +
-                            "\n- zaatakować z kopa lub bara (a)" +
-                            "\n- użyć ataku specjalnego (spa), ale czy masz na to siłę?" +
-                            "\n- użyj broni, nie bądź pizda! (ia)" +
-                            "\n- zregeneruj siły (b)");
+                            "\n-zaatakować z kopa lub bara (a)" +
+                            "\n-uzyć ataku specjalnego (spa), ale czy masz na to siłę?" +
+                            "\n-użyj broni, nie bądż pizda! (ia)" +
+                            "\n-zregeneruj siły (b)");
                     System.out.println("Chcesz dowiedzieć się więcej? Wpisz help ia/b (broń/przedmiot)");
                     break;
                 case "b":
                     System.out.println("Tu jest lista buffów do użytku xd");
                     break;
                 case "ia":
-                    System.out.println("Tu jest lista broni do użytku");
+                    System.out.println("tu jest lista broni do użytku");
                     break;
                 default:
-                    System.out.println("Błąd xd");
+                    System.out.println("błąd xd");
             }
-            return true;
-        };
-
-        if (helpCondition.test(x)) {
-            // Execute additional actions if the condition is met
+            return null;
         }
-
-        return this;
     }
-}
+
+
