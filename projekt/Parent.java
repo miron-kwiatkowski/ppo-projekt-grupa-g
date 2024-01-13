73a451eff8af07f56d694a0768ffe103b93cc91b
Ewakubera <146948857+Ewakubera@users.noreply.github.com>
2024-01-13 21:34:35 +0100

    Update Parent.java

--- projekt/Parent.java
+++ projekt/Parent.java
@@ -11,10 +11,11 @@ public class Parent extends Hobo {
         this.item = "Bombelek";
     }
 
-    public int getAttackPoints() {
-        return attackPoints;
-    }
-
+  public void specialAttack(Hobo target) {
+        System.out.println(getName() + " używa specjalnego ataku!");
+        target.takeHit(getAttackPoints() + 10); // Atak specjalny zwiększa obrażenia o 10
+        this.manaPoints -= 10; // Odejmuje 10 punktów many po użyciu specjalnego ataku
+      
     public void attack(Hobo target) {
         System.out.println(getName() + " wyzywa cię od nierobów\n-" + getAttackPoints() + " HP.");
         target.takeHit(getAttackPoints());
