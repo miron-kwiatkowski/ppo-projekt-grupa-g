15361b69fffbcfb9bb3d5ce2b70ae0f58f22fa4f
Amelia Wałek (HeroHorrendous) <147624792+AmeliaWalek@users.noreply.github.com>
2024-01-13 18:47:51 +0100

    Junkie.java

--- projekt/Junkie.java
+++ projekt/Junkie.java
@@ -12,7 +12,7 @@ public class Junkie extends Hobo {
     }
 
     public void attack(Hobo target) {
-        System.out.println(getName() + " Zaczął ci grozić\n-" + getAttackPoints() + " HP.");
+        System.out.println(getName() + " Grozi ci\n-" + getAttackPoints() + " HP.");
         target.takeHit(getAttackPoints());
     }
 
