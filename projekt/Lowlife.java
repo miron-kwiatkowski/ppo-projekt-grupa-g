451ae5b5a49d9c9b906b7226bd2167f8c4e1af31
Ewakubera <146948857+Ewakubera@users.noreply.github.com>
2024-01-13 21:34:21 +0100

    Update Lowlife.java

--- projekt/Lowlife.java
+++ projekt/Lowlife.java
@@ -9,9 +9,10 @@ public class Lowlife extends Hobo {
         this.item = "Kij";
     }
 
-    public int getAttackPoints() {
-        return attackPoints;
-    }
+  public void specialAttack(Hobo target) {
+        System.out.println(getName() + " używa specjalnego ataku!");
+        target.takeHit(getAttackPoints() + 10); // Atak specjalny zwiększa obrażenia o 10
+        this.manaPoints -= 10; // Odejmuje 10 punktów many po użyciu specjalnego ataku
 
     public void attack(Hobo target) {
         System.out.println(getName() + " daje ci z łokcia\n-" + getAttackPoints() + " HP.");
