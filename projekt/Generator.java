85376bdd76e8b50ddc765b2de8e233272cfadef7
Amelia Wałek (HeroHorrendous) <147624792+AmeliaWalek@users.noreply.github.com>
2024-01-13 18:24:20 +0100

    Update Generator.java

--- projekt/Generator.java
+++ projekt/Generator.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class Generator{
     private static final List<String> maleFirstNames = Arrays.asList("Mietek", "Ryszard", "Zdzichu", "Mścichuj", "Moczywór", "Andrzej", "Józek", "Stachu", "Bolek", "Marian", "Brajan", "Tadek", "Ferdek", "Bożydar");
-    private static final List<String> femaleFirstNames = Arrays.asList("Karyna", "Nikola", "Andżelika", "Dżeskia", "Elka", "Helena", "Halina", "Bożenka", "Jebodyda", "Mścisława", "Kaśka", "Klaudia", "Żanetka", "Renata", "Mariola");
+    private static final List<String> femaleFirstNames = Arrays.asList("Karyna", "Nikola", "Andżelika", "Dżesika", "Elka", "Helena", "Halina", "Bożenka", "Jebodyda", "Mścisława", "Kaśka", "Klaudia", "Żanetka", "Renata", "Mariola");
     private static final List<String> origin = Arrays.asList("spod Lidla", "spod Żabki", "spod Kościoła", "z rowu", "spod Biedronki", "spod śmietnika", "z zaułka", "z piwnicy", "spod mostu", "z mlekiem");
     
 }
