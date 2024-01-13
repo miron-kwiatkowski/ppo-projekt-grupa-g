9e0cef4f7692a4cacd5a00069d06aca9ffa48708
Amelia Wałek (HeroHorrendous) <147624792+AmeliaWalek@users.noreply.github.com>
2024-01-13 18:47:10 +0100

    Menel.java

---
+++ projekt/Menel.java
@@ -0,0 +1,23 @@
+public class Menel extends Hobo {
+    private int attackPoints;
+
+    public Patus(String name, String gender) {
+        super(name, gender);
+        this.className = "Menel";
+        this.attackPoints = 10;
+    }
+
+    public int getAttackPoints() {
+        return attackPoints;
+    }
+
+    public void attack(Hobo target) {
+        System.out.println(getName() + " Nacharał na ciebie\n-" + getAttackPoints() + " HP.");
+        target.takeHit(getAttackPoints());
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nAtakuje z siłą: " + getAttackPoints();
+    }
+}
