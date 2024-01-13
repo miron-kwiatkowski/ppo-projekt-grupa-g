09fe16935498704d9ae6d8779317f48c94d9b91e
Amelia Wałek (HeroHorrendous) <147624792+AmeliaWalek@users.noreply.github.com>
2024-01-13 19:13:59 +0100

    Help.java

--- projekt/Help.java
+++ projekt/Help.java
@@ -13,11 +13,11 @@ public class Help {
                 break;
             case "ia":
                 System.out.println("Tu jest lista broni do użytku, jeśli je posiadasz");
-                System.out.println("Klejpała - Wyjęta ze śmietnika, potężna laska kleju na gorąco. Idealna do pałowania przeciwników ");
-                System.out.println("Tulipan - ");
-                System.out.println("Bombelek - ");
-                System.out.println("Laczek - ");
-                System.out.println("Strzykawka - ");
+                System.out.println("Klejpała - Wyjęta ze śmietnika, potężna laska kleju na gorąco. Idealna do pałowania przeciwników. + 5 do ataku");
+                System.out.println("Tulipan - Rozbita o krawężnik pusta butelka po tanim winku. Broń krucha, ale jakże skuteczna. + 10 do ataku");
+                System.out.println("Bombelek - Amunicja wielokrotnego użytku - rzucasz, a po trafieniu w przeciwnika bachorek wraca. + 15 do ataku");
+                System.out.println("Kij - Nie stać cię na prawdziwy kij bejsbolowy? Nic starconego! Kij od mitły zadziała równie skutecznie. + 10 do ataku");
+                System.out.println("Strzykawka - Lepiej na nią uważać. Nie wiadomo ile osób korzystało z niej wcześniej. Roznosi HIV. + 15 do ataku");
                 break;
             default:
                 System.out.println("błąd xd");
