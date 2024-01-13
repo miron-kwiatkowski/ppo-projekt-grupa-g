cca1461a2750fc582e6092c90911270ca05f66f5
Amelia Wałek (HeroHorrendous) <147624792+AmeliaWalek@users.noreply.github.com>
2024-01-13 19:25:03 +0100

    Help.java

--- projekt/Help.java
+++ projekt/Help.java
@@ -7,9 +7,15 @@ public class Help {
                 // Print help information
                 break;
             case "b":
-                System.out.println("Tu jest lista buffów do użytku xd");
-                System.out.println("Batonik - ");
-                System.out.println("Amarenka - ");
+                System.out.println("Lista przedmiotów, które możesz zdobyć w trakcie rozgrywki:");
+                System.out.println("Batonik - Tani i w miarę pożywny. Ponosi poziom cukru. + 5 hp");
+                System.out.println("Pizza - Kawałek starej pizzy znaleziony przy śmietniku. Szkoda, że hawajska... + 20 hp");
+                System.out.println("Mortadela - Ulubiona szynka, która nie widziała mięsa w składzie. + 15 hp");
+                System.out.println("Bułka - Kajzerka z ulubionego dyskontu. 33 grosze, a tyle smaku. Idealna do mortadeli. + 10 hp");
+                System.out.println("Amarenka - Mirabelkowa Amarenka. Nic dodać, nic ująć. Pyszny smak, a jak sponiewiera! + 15 mp");
+                System.out.println("Winko - Najtańszy winiacz jaki tylko można było znaleźć. Nie dość mocny, aby upić się po butelce. + 5 mp");
+                System.out.println("Piwko - Jedyne słuszne - Mocny VIP za całe 1,99! Można się nayebać za dobre pieniądze. + 10 mp");
+                System.out.println("Wódeczka - Królowa wśród tanich alkoholi. Nie ważne czy to małpeczka czy półlitrówka, jej cudowna moc zawsze zostanie doceniona. + 20 mp");
                 break;
             case "ia":
                 System.out.println("Tu jest lista broni do użytku, jeśli je posiadasz");
@@ -20,7 +26,7 @@ public class Help {
                 System.out.println("Strzykawka - Lepiej na nią uważać. Nie wiadomo ile osób korzystało z niej wcześniej. Roznosi HIV. + 15 do ataku");
                 break;
             default:
-                System.out.println("błąd xd");
+                System.out.println("Błąd xD Nie umiesz pisać");
         }
         return this;
     }
