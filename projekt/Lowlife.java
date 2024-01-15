881cad672eb495a00fd18d1ac7dfdc1a1da8b249
Ewakubera <146948857+Ewakubera@users.noreply.github.com>
2024-01-15 21:03:08 +0100

    Update Lowlife.java

--- projekt/Lowlife.java
+++ projekt/Lowlife.java
@@ -1,19 +1,17 @@
 public class Lowlife extends Hobo {
-    private int attackPoints;
 
     public Lowlife(String name, String gender, String title) {
-        this.name = name;
-        this.gender = gender;
-        this.title = title;
-        this.className = "Patus";
-        this.attackPoints = 10;
-        this.item = "Kij";
+        super(name, gender);
+        setTitle("z bloku");
+        setClassName("Patus");
+        setAttackPoints(10);
+        setItem("Kij");
     }
 
   public void specialAttack(Hobo target) {
         System.out.println(getName() + " używa specjalnego ataku!");
         target.takeHit(getAttackPoints() + 10); // Atak specjalny zwiększa obrażenia o 10
-        this.manaPoints -= 10; // Odejmuje 10 punktów many po użyciu specjalnego ataku
+        takeManaPoints(10);  // Odejmuje 10 punktów many po użyciu specjalnego ataku
   }
 
     public void attack(Hobo target) {
@@ -23,6 +21,6 @@ public class Lowlife extends Hobo {
 
     @Override
     public String toString() {
-        return className + name + "\nAtakuje z siłą: " + getAttackPoints();
+        return super.toString();
     }
 }
