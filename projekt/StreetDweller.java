1288aa06e3c8285a2a9e2a80b7af58d63d63c2c0
Ewakubera <146948857+Ewakubera@users.noreply.github.com>
2024-01-15 21:04:48 +0100

    Update StreetDweller.java

--- projekt/StreetDweller.java
+++ projekt/StreetDweller.java
@@ -1,21 +1,17 @@
 public class StreetDweller extends Hobo {
-    private int attackPoints;
-    private String title;
-    private String className;
 
     public StreetDweller(String name, String gender, String title) {
-        this.name = name;
-        this.gender = gender;
-        this.title = title;
-        this.className = "Menel";
-        this.attackPoints = 10;
-        this.item = "Klejpała";
+        super(name, gender);
+        setTitle("z kerfura");
+        setClassName("Menel");
+        setAttackPoints(10);
+        setItem("Klejpała");
     }
 
   public void specialAttack(Hobo target) {
         System.out.println(getName() + " używa specjalnego ataku!");
         target.takeHit(getAttackPoints() + 10); // Atak specjalny zwiększa obrażenia o 10
-        this.manaPoints -= 10; // Odejmuje 10 punktów many po użyciu specjalnego ataku
+        takeManaPoints(10);  // Odejmuje 10 punktów many po użyciu specjalnego ataku
   }
 
     public void attack(Hobo target) {
@@ -25,6 +21,6 @@ public class StreetDweller extends Hobo {
 
     @Override
     public String toString() {
-        return className + name + "\nAtakuje z siłą: " + getAttackPoints();
+        return super.toString();
     }
 }
