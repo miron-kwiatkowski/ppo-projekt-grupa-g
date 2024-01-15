51866045203a6a054e40119170851494713474b2
Ewakubera <146948857+Ewakubera@users.noreply.github.com>
2024-01-15 21:06:09 +0100

    Update Parent.java

--- projekt/Parent.java
+++ projekt/Parent.java
@@ -1,21 +1,17 @@
 public class Parent extends Hobo {
-    private int attackPoints;
-    private String title;
-    private String className;
 
     public Parent(String name, String gender, String title) {
-        this.name = name;
-        this.gender = gender;
-        this.title = title;
-        this.className = "Dysfunkcjonalny rodzic";
-        this.attackPoints = 10;
-        this.item = "Bombelek";
+        super(name, gender);
+        setTitle("z Mopsu");
+        setClassName("Dysfunkcyjny rodzic");
+        setAttackPoints(10);
+        setItem("Bombelek");
     }
 
   public void specialAttack(Hobo target) {
         System.out.println(getName() + " używa specjalnego ataku!");
         target.takeHit(getAttackPoints() + 10); // Atak specjalny zwiększa obrażenia o 10
-        this.manaPoints -= 10; // Odejmuje 10 punktów many po użyciu specjalnego ataku
+        takeManaPoints(10); // Odejmuje 10 punktów many po użyciu specjalnego ataku
   }
       
     public void attack(Hobo target) {
@@ -25,6 +21,6 @@ public class Parent extends Hobo {
 
     @Override
     public String toString() {
-        return className + name + "\nAtakuje z siłą: " + getAttackPoints();
+        return super.toString();
     }
 }
