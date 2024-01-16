5fbe6c78a4bea5acba3999773cdabd887381e2d6
Ewakubera <146948857+Ewakubera@users.noreply.github.com>
2024-01-16 11:07:53 +0100

    Update Items.java

--- projekt/Items.java
+++ projekt/Items.java
@@ -16,24 +16,24 @@ public class Items {
 
     private void initializeItemActions() {
         itemActions = new HashMap<>();
-        itemActions.put("Klejpała", player -> {
+        itemActions.put("klejpała", player -> {
             System.out.println("Gracz rzuca klejpałą wygrzebaną ze śmietnika!");
             player.takeHit(5);
         });
-        itemActions.put("Tulipan", player -> {
+        itemActions.put("tulipan", player -> {
             System.out.println("Gracz uderza tulipanem z butelki po tanim winie!");
             player.takeHit(10);
         });
-        itemActions.put("Bombelek", player -> {
+        itemActions.put("bombelek", player -> {
             System.out.println("Gracz rzuca bombelka! Ma ich sporą amunicję dzięki 500+");
             player.takeHit(15);
         });
 
-        itemActions.put("Kij", player -> {
+        itemActions.put("kij", player -> {
             System.out.println("Gracz uderza kijem od miotły!");
             player.takeHit(10);
         });
-        itemActions.put("Strzykawka", player -> {
+        itemActions.put("strzykawka", player -> {
             System.out.println("Gracz używa zużytej strzykawki! Zadajesz większe obrażenia, ale dostajesz również HIV!");
             player.takeHit(15);
         });
@@ -43,41 +43,41 @@ public class Items {
 
     private void initializeBuffActions() {
         buffActions = new HashMap<>();
-        buffActions.put("Batonik", player -> {
+        buffActions.put("batonik", player -> {
             System.out.println("Gracz zjada batonika!");
             player.applyHealing(5);
         });
 
-        buffActions.put("Pizza", player -> {
+        buffActions.put("pizza", player -> {
             System.out.println("Gracz zjada starą pizzę!");
             player.applyHealing(20);
         });
 
-        buffActions.put("Mortadela", player -> {
+        buffActions.put("mortadela", player -> {
             System.out.println("Gracz zjada najtańszą mortadelę!");
             player.applyHealing(15);
         });
 
-        buffActions.put("Bułka", player -> {
+        buffActions.put("bułka", player -> {
             System.out.println("Gracz zjada tanią i pożywną kajzerkę!");
             player.applyHealing(10);
         });
 
-        buffActions.put("Winko", player -> {
+        buffActions.put("winko", player -> {
             System.out.println("Gracz pije tanie winko");
             player.applyManaRestoration(5);
         });
-        buffActions.put("Amarenka", player -> {
+        buffActions.put("amarenka", player -> {
             System.out.println("Gracz pije mirabelkową amarenkę");
             player.applyManaRestoration(15);
         });
 
-        buffActions.put("Piwko", player -> {
+        buffActions.put("piwko", player -> {
             System.out.println("Gracz pije najtańsze piwko z Biedronki - Piwo VIP, za całe 1,99 za puszkę!");
             player.applyManaRestoration(10);
         });
 
-        buffActions.put("Wódeczka", player -> {
+        buffActions.put("wódeczka", player -> {
             System.out.println("Gracz pije napój bogów - wodę ognistą!");
             player.applyManaRestoration(20);
         });
