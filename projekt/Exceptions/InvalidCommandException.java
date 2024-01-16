79249fb8fca75fc1c44f3678557346fc1745ae05
miron-kwiatkowski <147618177+miron-kwiatkowski@users.noreply.github.com>
2024-01-16 21:43:19 +0100

    Update InvalidCommandException.java

--- projekt/Exceptions/InvalidCommandException.java
+++ projekt/Exceptions/InvalidCommandException.java
@@ -1,7 +1,7 @@
 package Exceptions;
 public class InvalidCommandException extends Exceptions.InputException {
   public InvalidCommandException() {
-    super("Nie umiesz pisać xD Spróbuj jeszcze raz, może się uda."););
+    super("Nie umiesz pisać xD Spróbuj jeszcze raz, może się uda.");
   }
 
   @Override
