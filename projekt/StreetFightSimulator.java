b66eb5192bc44044575e681b535535698dd552f7
Ewakubera <146948857+Ewakubera@users.noreply.github.com>
2024-01-16 22:32:32 +0100

    Update StreetFightSimulator.java

--- projekt/StreetFightSimulator.java
+++ projekt/StreetFightSimulator.java
@@ -2,71 +2,74 @@ import java.util.Scanner;
 import Exceptions.*;
 
 public class StreetFightSimulator {
-            public static void main(String[] args) throws GenderException, InvalidCommandException {
-                Scanner input = new Scanner(System.in);
-                System.out.println("Witaj w symulatorze walk ulicznych! " +
-                        "\nCzeka cię wielkie wyzwanie stoczenia boju z najwybitniejszymi jednostkami naszego miasta. \n" +
-                        "Pora abyś powiedział nam kim jesteś.");
-                try {
-                    System.out.println("Podaj swoje imię przybyszu:");
-                    String name = input.next();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-                    System.out.println("Podaj swoją płeć (m/k):");
-                    String gender = input.next();
+        try {
+            System.out.println("Witaj w symulatorze walk ulicznych! " +
+                    "\nCzeka cię wielkie wyzwanie stoczenia boju z najwybitniejszymi jednostkami naszego miasta. \n" +
+                    "Pora abyś powiedział nam kim jesteś.");
 
-                    if (!"m".equals(gender) && !"k".equals(gender)) {
-                        input.close();
-                        throw new GenderException();
-                    }
+            System.out.println("Podaj swoje imię przybyszu:");
+            String name = input.next();
 
-                    String title = Generator.generateRandomTitle();
-                    Hobo player = new Hobo(name, gender, title);
-                    System.out.println("\n\nWitaj, " + name + "! Cieszę się, że dołączasz do symulatora walk ulicznych.\nOd dzisiaj zwiesz się:");
-                    System.out.println(player.toString());
+            System.out.println("Podaj swoją płeć (m/k):");
+            String gender = input.next();
 
-                    System.out.println("\nPora na spokojną przechadzkę po naszym pięknym wygwizdowie. " +
-                            "\nAle coż to, twój pierwszy przeciwnik wyłania się zza winkla!");
-                    // ROZPOCZNIJ WALKĘ
-                    int battles = 0;
-                    while (!player.isDead()) {
-                        if (battles == 3) {
-                            ChurchDoorkeeper churchDoorkeeper = new ChurchDoorkeeper();
-                            Satanist satanist = new Satanist();
-                            System.out.println("\nMijasz kościół, zastanawiasz się czy woda święcona ma w sobie procenty. Gdy już zamierzasz podpić wodę z kościelnej misy, napotykasz kogoś kto nie chce dzielić się \"procentami\".");
-                            Fight.bossFight(player,churchDoorkeeper);
-                            if (!player.isDead()) {
-                                player.applyHealing(30);
-                                player.applyManaRestoration(30);
-                                System.out.println("Udało ci się przeżyć! Oto twoja nagroda: +30 HP, +30 MP");
-                                System.out.println("Ale cóż to! Odźwierny zaczął przechodzić transformację! Czy to sam Szatan w niego wstąpił!");
-                                Fight.bossFight(player,satanist);
-                                System.out.println("\n\nGratulacje "+player.getName()+", udało ci się przetrwać spokojny niedzielny spacer po naszym blokowisku.");
-                                break;
-                            }
-                        } else {
-                            Fight.fight(player);
-                        }
-                        if (!player.isDead()) {
-                            player.applyHealing(30);
-                            player.applyManaRestoration(30);
-                            System.out.println("Udało ci się przeżyć! Oto twoja nagroda: +30 HP, +30 MP");
-                            battles++;
-                        }
-                        else if(player.isDead()){
-                            System.out.println("\n\nNo, było blisko, ale ostatecznie nie udało ci się pokonać każdego przeciwnika na tym jakże spokojnym blokowisku.");
-                        }
-                    }
-                    System.out.print("Dziękujemy, że ");
-                            if(player.getGender().equals("m")){
-                                System.out.print("zechciałeś ");
-                            }
-                            else if(player.getGender().equals("k")){
-                                System.out.print("zechciałaś ");
-                            }
-                            System.out.print("spróbować swoich sił w zaciekłej walce :>\n\nTwórcy gry:\nEwa Kubera\nMiron Kwiatkowski\nAmelia Wałek\nPatrycja Marucińska\n\nMamy nadzieję, że rozgrywka wywołała chociaż lekki uśmiech ;D\n");
-                } catch (GenderException | InvalidCommandException | UnknownItemException e) {
-                    System.out.println("Błąd: " + e.getMessage());
-                }
-                input.close();
+            if (!"m".equals(gender) && !"k".equals(gender)) {
+                throw new GenderException();
             }
+
+            String title = Generator.generateRandomTitle();
+            Hobo player = new Hobo(name, gender, title);
+            System.out.println("\n\nWitaj, " + name + "! Cieszę się, że dołączasz do symulatora walk ulicznych.\nOd dzisiaj zwiesz się:");
+            System.out.println(player.toString());
+
+            System.out.println("\nPora na spokojną przechadzkę po naszym pięknym wygwizdowie. " +
+                    "\nAle coż to, twój pierwszy przeciwnik wyłania się zza winkla!");
+
+            int battles = 0;
+
+            while (!player.isDead()) {
+                if (battles == 3) {
+                    ChurchDoorkeeper churchDoorkeeper = new ChurchDoorkeeper();
+                    Satanist satanist = new Satanist();
+                    System.out.println("\nMijasz kościół, zastanawiasz się czy woda święcona ma w sobie procenty. Gdy już zamierzasz podpić wodę z kościelnej misy, napotykasz kogoś kto nie chce dzielić się \"procentami\".");
+                    Fight.bossFight(player, churchDoorkeeper);
+                    if (!player.isDead()) {
+                        player.applyHealing(30);
+                        player.applyManaRestoration(30);
+                        System.out.println("Udało ci się przeżyć! Oto twoja nagroda: +30 HP, +30 MP");
+                        System.out.println("Ale cóż to! Odźwierny zaczął przechodzić transformację! Czy to sam Szatan w niego wstąpił!");
+                        Fight.bossFight(player, satanist);
+                        System.out.println("\n\nGratulacje " + player.getName() + ", udało ci się przetrwać spokojny niedzielny spacer po naszym blokowisku.");
+                        break;
+                    }
+                } else {
+                    Fight.fight(player);
+                }
+                if (!player.isDead()) {
+                    player.applyHealing(30);
+                    player.applyManaRestoration(30);
+                    System.out.println("Udało ci się przeżyć! Oto twoja nagroda: +30 HP, +30 MP");
+                    battles++;
+                } else {
+                    System.out.println("\n\nNo, było blisko, ale ostatecznie nie udało ci się pokonać każdego przeciwnika na tym jakże spokojnym blokowisku.");
+                    break; // Dodajemy break, aby przerwać pętlę, gdy gracz przegra walkę
+                }
+            }
+
+            System.out.print("Dziękujemy, że ");
+            if (player.getGender().equals("m")) {
+                System.out.print("zechciałeś ");
+            } else if (player.getGender().equals("k")) {
+                System.out.print("zechciałaś ");
+            }
+            System.out.print("spróbować swoich sił w zaciekłej walce :>\n\nTwórcy gry:\nEwa Kubera\nMiron Kwiatkowski\nAmelia Wałek\nPatrycja Marucińska\n\nMamy nadzieję, że rozgrywka wywołała chociaż lekki uśmiech ;D\n");
+        } catch (GenderException | InvalidCommandException | UnknownItemException e) {
+            System.out.println("Błąd: " + e.getMessage());
+        } finally {
+            input.close();
+        }
+    }
 }
